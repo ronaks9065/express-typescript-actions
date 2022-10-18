57b8e52bea824548f2ed984b3ef8ed6351d6afb7
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2022-10-18 10:22:25 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World from Express + TypeScript!!!, we are getting good at this!");
+    res.send("Hello From NodeJS");
 });
 
 app.listen(port, () => {
